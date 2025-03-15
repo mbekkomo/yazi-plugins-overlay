de59d36d6ec8b0ac4b484af94c819c2bde2bd1ce
Komo <afiqquraisyzulkarnain@gmail.com>
2025-03-15 10:20:08 +0800

    fix install

--- plugins/sudo-demo-yazi/default.nix
+++ plugins/sudo-demo-yazi/default.nix
@@ -13,6 +13,11 @@ mkYaziPlugin {
     rev = "2bf70d880e02db95394de360668325b46f804791";
     hash = "sha256-0A5UVbrP9+GRvX14VQm4Yxw+P9Ca5gtlk9qkLCVf5+Q=";
   };
+  
+  postInstall = ''
+    rm $SRCDIR/LICENSE
+    cp LICENSE $SRCDIR/LICENSE
+  '';
 
   srcdir = "sudo-demo.yazi";
 }
